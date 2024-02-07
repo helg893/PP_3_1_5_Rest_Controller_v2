f07ffb23dcb241c36713560f25e29815694b4992
helg893 <helg893@gmail.com>
2024-02-07 22:49:48 +0300

    sql

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -1,5 +1,7 @@
 package ru.kata.spring.boot_security.demo.models;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,24 +16,27 @@ import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
+
+    @Column(name = "username")
+    @NotEmpty(message = "поле не должно быть пустым")
+    private String username;
+
     @Column(name = "name")
-    @NotEmpty(message = "поле не должно быть пустым")
-    @Size(min = 2, max = 30, message = "длина должна быть от 2 до 30 символов")
     private String name;
+
     @Column(name = "surname")
-    @NotEmpty(message = "поле не должно быть пустым")
-    @Size(min = 2, max = 30, message = "длина должна быть от 2 до 30 символов")
     private String surname;
+
     @Column(name = "email")
     @NotEmpty(message = "поле не должно быть пустым")
     @Email(message = "должен быть корректный адрес e-mail")
     private String email;
-
 
     public User() {}
     public User(String name, String surname, String email) {
@@ -40,46 +45,4 @@ public class User {
         this.email = email;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
