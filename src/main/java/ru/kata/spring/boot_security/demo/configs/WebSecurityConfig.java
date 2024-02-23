8f13ace72fa00be9b57594299fbb294310f39d41
helg893 <helg893@gmail.com>
2024-02-23 19:20:56 +0300

    get /api/users get /api/users/{id} post /api/users put /api/users delete /api/users/{id} rest service ready

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +14,11 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable) // TODO: must to be enabled?
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/", "/api/**").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                        .requestMatchers("/", "/api/users/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersRestController.java
@@ -1,14 +1,14 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.exception_handling.NoSuchUserException;
-import ru.kata.spring.boot_security.demo.exception_handling.UserIncorrectData;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
@@ -35,5 +35,24 @@ public class UsersRestController {
         ));
     }
 
+    @PostMapping("/users")
+    public User addNewUser(@RequestBody User user) {
+        userService.save(user);
+        return user;
+    }
 
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user) {
+        userService.save(user);
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable long id) {
+        userService.findById(id).orElseThrow(() -> new NoSuchUserException(
+                String.format("There is no user with id=%s exists", id)
+        ));
+        userService.deleteById(id);
+        return String.format("User with id=%s was successfully removed", id);
+    }
 }
