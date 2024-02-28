825544907d936ad0b2df24986de316428c71bdba
helg893 <helg893@gmail.com>
2024-02-28 14:34:11 +0300

    +delete+edit

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -114,6 +114,7 @@ async function getAllRoles() {
 async function showAllRoles() {
     const roles = await getAllRoles()
     const newUserRoles = document.getElementById("newUserRoles")
+    newUserRoles.innerHTML = ""
     for (let key in roles) {
         newUserRoles.innerHTML += `
             <option id="${roles[key].id}" value="${roles[key].name}">${roles[key].shortName}</option>
@@ -263,8 +264,6 @@ formNewUser.addEventListener("submit",  async (event) =>  {
     } catch (e) {
         alert("fetching new user unexpected error: " + e)
     }
-
-
 })
 
 const newUserUsername = document.getElementById("newUserUsername")
@@ -311,29 +310,182 @@ async function getUser(id) {
     }
 }
 
-const formEditUser = document.getElementById("formEditUser")
+let formEditUser = document.getElementById("formEditUser")
 const modalEdit = document.getElementById("modalEdit")
+
 modalEdit.addEventListener("show.bs.modal", event => {
+    const showAllRoles = async () => {
+        const roles = await getAllRoles()
+        formEditUser.roles.innerHTML = ""
+        for (let role of roles) {
+            formEditUser.roles.innerHTML += `<option id="${role.id}" value="${role.name}">${role.shortName}</option>`
+        }
+    }
+    showAllRoles()
+
     const showUser = async () => {
         const user = await getUser(event.relatedTarget.dataset.id)
-        console.log(user)
-        console.log(user.username)
-        console.log(user.roles)
         formEditUser.id.value = user.id
         formEditUser.username.value = user.username
         formEditUser.name.value = user.name
         formEditUser.surname.value = user.surname
         formEditUser.email.value = user.email
         formEditUser.age.value = user.age
-
+        for (let option of formEditUser.roles) {
+            for (let role of user.roles) {
+                if (option.id == role.id) {
+                    option.selected = true
+                }
+            }
+        }
     }
     showUser()
-    // console.log(event.relatedTarget.dataset.id)
-    // console.log(event.relatedTarget.dataset.username)
-    // console.log(event.relatedTarget.dataset.name)
-    // console.log(event.relatedTarget.dataset.surname)
-    // console.log(event.relatedTarget.dataset.email)
-    // formEditUser.id.value = event.relatedTarget.dataset.id
-    // formEditUser.username.value = event.relatedTarget.dataset.username
-    // formEditUser.surname.value = event.relatedTarget.dataset.surname
+
+    const modalDivPassword = document.getElementById("modalDivPassword")
+    const modalHeaderLabel = document.getElementById("modalHeaderLabel")
+    const modalButtonSubmit = document.getElementById("modalButtonSubmit")
+    if (event.relatedTarget.dataset.operation === "delete") {
+        formEditUser.setAttribute("method", "DELETE")
+        // formEditUser.method = "DELETE"
+        formEditUser.password.value="pwd"
+        modalDivPassword.style.display = "none"
+        formEditUser.username.disabled = true
+        formEditUser.name.disabled = true
+        formEditUser.surname.disabled = true
+        formEditUser.email.disabled = true
+        formEditUser.age.disabled = true
+        formEditUser.roles.disabled = true
+        modalButtonSubmit.textContent = "Delete"
+        modalHeaderLabel.textContent = "Delete user"
+    } else if (event.relatedTarget.dataset.operation === "edit") {
+        formEditUser.setAttribute("method", "PUT")
+        // formEditUser.method = "PATCH"
+        modalDivPassword.style.display = ""
+        formEditUser.username.disabled = false
+        formEditUser.name.disabled = false
+        formEditUser.surname.disabled = false
+        formEditUser.email.disabled = false
+        formEditUser.age.disabled = false
+        formEditUser.roles.disabled = false
+        modalButtonSubmit.textContent = "Edit"
+        modalHeaderLabel.textContent = "Edit user"
+    } else {
+        alert("unexpected user operation value")
+    }
+})
+
+const modalUserId = document.getElementById("modalUserId")
+formEditUser.addEventListener("submit", async (event)=>{
+    event.preventDefault()
+
+    if (formEditUser.getAttribute("method") === "DELETE") {
+        try {
+            const response = await fetch(urlUsers + "/" + formEditUser.id.value, {
+                method: "DELETE"
+            })
+
+            if (response.ok) {
+                showAllUsers()
+                document.getElementById("modalButtonClose").click()
+            } else {
+                alert("fetch-delete user id=" + formEditUser.id.value + " unexpected response:"
+                    + response.status + " " + response.statusText)
+            }
+        } catch (e) {
+            alert("fetch-delete user id=" + formEditUser.id.value + " unexpected error: " + e)
+        }
+
+    } else if (formEditUser.getAttribute("method") === "PUT") {
+        const editUserRoles = document.getElementById("editUserRoles")
+        let roles = []
+
+        for (const option of editUserRoles.selectedOptions) {
+            let r = {
+                id: option.id,
+                name: option.value
+            }
+            roles.push(r)
+        }
+
+        const formData = new FormData(formEditUser)
+        let object = {
+            id: modalUserId.value,
+            roles: roles
+        }
+        formData.forEach((value, key) => {
+            // Reflect.has in favor of: object.hasOwnProperty(key)
+            if(!Reflect.has(object, key)){
+                object[key] = value;
+            }
+
+        });
+
+        try {
+            const response = await fetch(urlUsers, {
+                method: "PUT",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+
+            if (response.ok) {
+                formEditUser.reset()
+                showAllUsers()
+                document.getElementById("modalButtonClose").click()
+            } else {
+                alert("fetch-edit user id=" + modalUserId.value + " unexpected response:"
+                    + response.status + " " + response.statusText)
+            }
+        } catch (e) {
+            alert("fetch-edit user id=" + modalUserId.value + " unexpected error: " + e)
+        }
+
+    } else {
+        alert("modal form submit: unexpected user operation value")
+    }
+
+})
+
+const modalUsername = document.getElementById("modalUsername");
+modalUsername.addEventListener("input", ()=> {
+    const checkIfUsernameExists = async () => {
+        const users = await getAllUsers()
+        modalUsername.setCustomValidity("")
+        for (const user of users) {
+            if (user.id != modalUserId.value && modalUsername.value.toLowerCase() === user.username.toLowerCase()) {
+                modalUsername.setCustomValidity("another user with this username exists. please enter new username")
+                break
+            }
+            // if (user.id != modalUserId.value) {
+            //     if (modalUsername.value === user.username) {
+            //         modalUsername.setCustomValidity("another user with this username exists. please enter new username")
+            //         break
+            //     }
+            // }
+        }
+    }
+    checkIfUsernameExists()
+})
+
+const modalUserEmail = document.getElementById("modalUserEmail")
+modalUserEmail.addEventListener("input", ()=> {
+    const checkIfUserEmailExists = async () => {
+        const users = await getAllUsers()
+        modalUserEmail.setCustomValidity("")
+        for (const user of users) {
+            if (user.id != modalUserId.value && modalUserEmail.value.toLowerCase() === user.email.toLowerCase()) {
+                modalUserEmail.setCustomValidity("another user with this email exists. please enter new email")
+                break
+            }
+            // if (user.id != modalUserId.value) {
+            //     if (modalUserEmail.value === user.email) {
+            //         modalUserEmail.setCustomValidity("another user with this email exists. please enter new email")
+            //         break
+            //     }
+            // }
+        }
+    }
+    checkIfUserEmailExists()
 })
